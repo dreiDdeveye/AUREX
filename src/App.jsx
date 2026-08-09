9b50a1f71f5d085d3be4c03b2757a235a0bf245d
drei <paulandreivicente@gmail.com>
2026-08-10 00:36:52 +0800

    Change CA_PLACEHOLDER to a specific value
    Updated CA_PLACEHOLDER value to a specific string.

--- src/App.jsx
+++ src/App.jsx
@@ -509,7 +509,7 @@ function LeftSidebar({ view, go, mobileOpen, closeMobile, onRegister }) {
 
 function TopBar({ title, onSearch, onBell, notifCount, onMenu, onBack, showBack }) {
   const [copiedCA, setCopiedCA] = useState(false);
-  const CA_PLACEHOLDER = "N/A";
+  const CA_PLACEHOLDER = "5vd5CRH9gCHS61JquQ9YdoLpSFGEuBUWU9CU1SHCpumpA";
 
   const copyCA = async () => {
     try {
